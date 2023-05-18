f3b715a00203b6bc6898a4fc3175650a48fec0bf
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-05-18 08:52:26 -0500

    add code and test it

--- cript_graph/core.py
+++ cript_graph/core.py
@@ -1,5 +1,44 @@
+import cript
 import networkx as nx
+from math import sqrt
 
+def _determine_darkness_from_hex(color):
+    """
+    Determine the darkness of a color from its hex string.
+
+    Arguments:
+    ----------
+    color: str
+       7 character string with prefix `#` followed by RGB hex code.
+
+    Returns: bool
+       if the darkness is below half
+    """
+    # If hex --> Convert it to RGB: http://gist.github.com/983661
+    assert color[0] == "#"
+    red = int(color[1:3], 16)
+    green = int(color[3:5], 16)
+    blue = int(color[5:7], 16)
+    # HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+    hsp = sqrt(0.299 * red**2 + 0.587 * green**2 + 0.114 * blue**2)
+    return hsp < 127.5
+
+
+CRIPT_colors = {
+    "Group": "#775A55",
+    "User": "#BFB2AB",
+    "Project": "#20262C",
+    "Collection": "#576575",
+    "Experiment": "#ADAFBD",
+    "Inventory": "#E5E6EB",
+    "Material": "#275497",
+    "Process": "#951919",
+    "Data": "#2D9742",
+    "Computation": "#FFCC00",
+    "ComputationProcess": "#593196",
+    "Reference": "#FFFFFF",
+    "Software": "#FFFFFF",
+}
 
 def get_networkx_graph(root_node) -> nx.DiGraph:
     """
@@ -18,10 +57,32 @@ def get_networkx_graph(root_node) -> nx.DiGraph:
          The nodes are the actual python objects and no further node attributes are available.
          Edges carry an `attribute` that labels the attribute of the parent node which connects them.
     """
-    pass
+    def add_node(G, node):
+        """
+        Helper function, that adds a node and all its edges to graph.
+        Calls itself recursively on all BaseNodes.
+        """
+        if node in G.nodes:
+            return G
+        G.add_node(node)
+        for field in node._json_attrs.__dataclass_fields__:
+            attr_list = getattr(node._json_attrs, field)
+            if not isinstance(attr_list, list):
+                attr_list = [attr_list]
+            for attr in attr_list:
+                if isinstance(attr, cript.nodes.core.BaseNode):
+                    G = add_node(G, attr)
+                    if node != attr:
+                        G.add_edge(node, attr, attribute=field)
+        return G
+
+    G = nx.DiGraph()
+    add_node(G, root_node)
+    return G
 
 
-def get_dot_graph(graph, include_in_label=["node", "name", "key"]):
+
+def get_dot_graph(graph, include_in_label=["node_type", "name", "key"]):
     """
     Converts a networkx representation (see `get_networkx_graph`) of a CRIPT data graph
     into a description of the Graphiz Dot language (https://www.graphviz.org/doc/info/lang.html).
@@ -42,4 +103,25 @@ def get_dot_graph(graph, include_in_label=["node", "name", "key"]):
     --------
     String in the dot language that can be used to invoke the graph vizualization via graphiz tools.
     """
-    pass
+    dot_str = "strict digraph { \n"
+    for node in graph.nodes:
+        label = ""
+        for name in include_in_label:
+            try:
+                label += f"{getattr(node, name)} "
+            except AttributeError:
+                pass
+            extra_attr = ""
+        if node.node_type in CRIPT_colors:
+            color = CRIPT_colors[node.node_type]
+            extra_attr = f'style=filled, fillcolor="{color}", '
+            if _determine_darkness_from_hex(color):
+                extra_attr += "fontcolor=white,"
+
+        dot_str += f'"{node.uid}" [{extra_attr} label="{label}"];\n'
+
+    for edge in graph.edges():
+        edge_data = graph.get_edge_data(*edge)
+        dot_str += f"\"{edge[0].uid}\" -> \"{edge[1].uid}\" [label={edge_data['attribute']}];\n"
+    dot_str += "}\n"
+    return dot_str
--- tests/test_import.py
+++
@@ -1,7 +0,0 @@
-# trunk-ignore-all(ruff/F401)
-
-import cript_graph
-
-
-def test_import():
-    pass
